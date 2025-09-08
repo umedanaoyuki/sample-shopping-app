03e15347f06373a7526c8018a35aaf02d97d11e3
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-09-08 23:17:50 +0900

    feat: 文字を上下左右中央に配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="">
-      <main className="flex flex-col">
-        <p>Hello World</p>
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <main>
+        <h1 className="text-5xl font-bold">Hello World</h1>
       </main>
     </div>
   );
